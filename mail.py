c7375125c219578c7b06bfa706b923d0ef1de895
Sanidhya-Sinha <64468053+Sanidhya-Sinha@users.noreply.github.com>
2020-05-25 18:56:06 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,17 @@
+import smtplib
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+
+s.login("sanidhyasinha2000@gmail.com", "welcomedam")
+
+
+    # message
+message = "Your accuracy is less than 90% :("
+
+
+    # sending the mail
+s.sendmail("sanidhyasinha2000@gmail.com", "1706355@kiit.ac.in", message)
+
+
+    # terminating the session
+s.quit()
---
+++ mail_success.py
@@ -0,0 +1,17 @@
+import smtplib
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+
+s.login("sanidhyasinha2000@gmail.com", "welcomedam")
+
+
+    # message
+message_success = "Achieved your desired accuracy without tweeking . Congrats :)"
+
+
+    # sending the mail
+s.sendmail("sanidhyasinha2000@gmail.com", "1706355@kiit.ac.in", message_success)
+
+
+    # terminating the session
+s.quit()
