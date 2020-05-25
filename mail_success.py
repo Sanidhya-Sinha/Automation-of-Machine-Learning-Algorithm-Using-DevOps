25156053d1ea07603a9d61dd1bda93b33d3bb676
Sanidhya-Sinha <64468053+Sanidhya-Sinha@users.noreply.github.com>
2020-05-25 21:31:36 +0530

    Update mail_success.py

--- mail_success.py
+++ mail_success.py
@@ -2,7 +2,7 @@ import smtplib
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
 
-s.login("sanidhyasinha2000@gmail.com", "welcomedam")
+s.login("sanidhyasinha2000@gmail.com", "**********")
 
 
     # message
